511c1047e31a3ab68611e63967be1cb9bae993a9
Spencer Judge <sjudge@hey.com>
2020-11-29 21:50:12 -0800

    Forgot to add dir.

---
+++ state_machine_trait/src/lib.rs
@@ -0,0 +1,16 @@
+use std::error::Error;
+
+/// This trait defines a state machine (more formally, a [finite state
+/// transducer](https://en.wikipedia.org/wiki/Finite-state_transducer)) which accepts events (the
+/// input alphabet), uses them to mutate itself, and (may) output some commands (the output
+/// alphabet) as a result.
+pub trait StateMachine<State, Event, Command> {
+    /// The error type produced by this state machine when handling events
+    type Error: Error;
+
+    /// Handle an incoming event
+    fn on_event(&mut self, event: Event) -> Result<Vec<Command>, Self::Error>;
+
+    /// Returns the current state of the machine
+    fn state(&self) -> &State;
+}
